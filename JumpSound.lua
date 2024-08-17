da394c69e90d2c30d37c9d87a4ccfab972ee8ef7
Matan <matan.yehezk@icloud.com>
2024-08-17 23:20:46 +0300

    JumpSound.lua

---
+++ JumpSound.lua
@@ -0,0 +1,17 @@
+-- Make sure to put this script in StarterPlayer -> StarterPlayerScripts
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+local jumpSound = Instance.new("Sound", character)
+jumpSound.SoundId = "rbxassetid://2428506580"
+jumpSound.Volume = 0.3
+local function onJumping()
+	jumpSound:Play()
+end
+
+humanoid.Jumping:Connect(onJumping)
